5f8bb8647915be8f6514592026384c3fb8c25c31
Fortfel <32614998+Fortfel@users.noreply.github.com>
2026-03-01 14:41:37 +0100

    feat: added categories page

---
+++ apps/tanstack-start/src/routes/_app/_public/shop/$category.tsx
@@ -0,0 +1,54 @@
+import { createFileRoute, notFound } from '@tanstack/react-router'
+
+import { Button } from '@workspace/ui/components/button'
+
+import type { CategorySlug } from '~/routes/_app/-categories'
+import { CATEGORY_SLUGS } from '~/routes/_app/-categories'
+import { SHOP_DATA } from '~/routes/_app/-data'
+
+export const Route = createFileRoute('/_app/_public/shop/$category')({
+  loader: ({ params }) => {
+    if (!Object.values(CATEGORY_SLUGS).includes(params.category as CategorySlug)) {
+      notFound({ throw: true })
+    }
+  },
+  component: RouteComponent,
+  notFoundComponent: () => <div>This page does not exist</div>,
+})
+
+function RouteComponent() {
+  const { category } = Route.useParams()
+
+  return (
+    <div>
+      <h1 className="py-8 text-center text-4xl font-bold uppercase">{category}</h1>
+      <div className="grid grid-cols-1 gap-8 space-y-4 sm:grid-cols-2 lg:grid-cols-3">
+        {SHOP_DATA.map(
+          (cat) =>
+            cat.title === category &&
+            cat.items.map((item) => (
+              <div key={item.id} className="group relative">
+                <img
+                  className="aspect-square w-full object-cover group-hover:opacity-80"
+                  src={item.imageUrl}
+                  alt={item.name}
+                />
+                <div className="flex justify-between">
+                  <h3>{item.name}</h3>
+                  <p>{item.price}</p>
+                </div>
+                <Button
+                  size="lg"
+                  variant="secondary"
+                  className="hover:bg-secondary-foreground hover:text-secondary absolute bottom-10 left-1/2 -translate-x-1/2 cursor-pointer opacity-100 group-hover:opacity-100 sm:opacity-0"
+                  asChild
+                >
+                  <a>ADD TO CART</a>
+                </Button>
+              </div>
+            )),
+        )}
+      </div>
+    </div>
+  )
+}
